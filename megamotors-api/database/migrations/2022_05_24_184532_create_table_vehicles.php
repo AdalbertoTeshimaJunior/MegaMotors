0985dd2b6f873b5b24e094af3b9b0355feaa8628
Adalberto Teshima Júnior <teshima.jr@hotmail.com>
2022-05-25 10:19:23 -0300

    Adding vehicle migration and vehicle class

---
+++ megamotors-api/database/migrations/2022_05_24_184532_create_table_vehicles.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('veiculo');
+            $table->string('marca');
+            $table->integer('ano');
+            $table->text('descricao');
+            $table->boolean('vendido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
